bce301d94a1dc8d0ebff12cf9ca6572fa2c23fe0
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2023-05-04 14:49:03 +0530

    lab 8 updates

--- week8/heap.cpp
+++ week8/heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
   
 // function to heapify the tree
@@ -9,21 +10,23 @@ void heapify(int arr[], int n, int root)
 
    int largest = root;
 
-   if(root<n && arr[left]>arr[largest])
+   if(left<n && arr[left]>arr[largest]){ 
+      largest = left;
+      }
+
+   if(right<n && arr[right]>arr[largest]) {
+      largest = right;
+      }
+
+   if(largest != root){
+      swap(arr[root], arr[largest]);
+
+      heapify(arr, n, largest);
+   }
   
    
 }
-  
-// implementing heap sort
-void heapSort(int arr[], int n)
-{
-   // build heap
-   
-  
-   // extracting elements from heap one by one
-   
-}
-  
+
 /* print contents of array */
 void displayArray(int arr[], int n)
 {
@@ -32,11 +35,57 @@ void displayArray(int arr[], int n)
    cout << "\n";
 }
   
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+
+   //create the heap
+   for(int i=n/2 -1; i>=0; i--){
+      heapify(arr, n, i);
+   }
+
+   displayArray(arr, n);
+   
+  
+   
+      //select the maximum items iteratively and swap them to the end
+       for (int i = n - 1; i > 0; i--) {
+ 
+   
+        swap(arr[0], arr[i]);
+ 
+        
+        heapify(arr, i, 0);
+    }
+   
+}
+  
+
 // main program
 int main()
 {
-   int heap_arr[] = {4,17,3,12,9,6};
-   int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
+   
+   
+   //create a vector to store the user inputs
+   vector<int> arr;
+   string input;
+    
+    cout << "Enter your numbers seperated by white spaces and press enter: ";
+    getline(cin, input);
+   //conveting the numbers into a string stream
+    istringstream is(input);
+    int num;
+
+    while(is >> num)
+        arr.push_back(num);
+   int n = arr.size();
+
+   int heap_arr[n];
+
+   for(int i=0; i<n;i++){
+      heap_arr[i] = arr[i];
+   }
    cout<<"Input array"<<endl;
    displayArray(heap_arr,n);
   
@@ -44,4 +93,5 @@ int main()
   
    cout << "Sorted array"<<endl;
    displayArray(heap_arr, n);
+   return 0;
 }
